a9d1136ecaccfb3428a2abfda5b6578160b55748
Huskey <vladislav.bychkov01@gmail.com>
2020-08-16 01:08:13 +0300

    update fields and more

--- my-letter-social/db/models.js
+++
@@ -1,9 +0,0 @@
-import uuid from 'uuid/v4';
-
-export class Post {
-    constructor(config) {
-        this.id = config.id || uuid();
-        this.comment = config.comment || [];
-        this.date = config.date || new Date().getTime();
-    }
-}
