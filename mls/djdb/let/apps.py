5cf30d9f511dd19aeaf4b907d42500b4fe0a37ae
Huzzky <vladislav.bychkov01@gmail.com>
2020-08-30 03:04:55 +0300

    removal of unnecessary

--- mls/djdb/let/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LetConfig(AppConfig):
-    name = 'let'
--- mls/frontReact/src/history/history.js
+++
@@ -1,4 +0,0 @@
-import {createBrowserHistory} from 'history'
-const history = createBrowserHistory();
-const navigate = to => history.push(to);
-export { history, navigate };
